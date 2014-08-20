5d6b571a16cf8ea13e7fa7e03185808926b3ffad
Tyson Frederick <tkinetik1@gmail.com>
2014-08-20 12:22:23 -0700

    Create snippets-sum-fresvii.c

---
+++ snippets-sum-fresvii.c
@@ -0,0 +1,23 @@
+        int y = j;
+        int z = k;
+        int a = l;
+        int b = m;
+        int c = o;
+        
+        /*        for (int j = i + 1 , n = strlen(s); i <= n; i++)*/
+/*        {*/
+
+/*        }*/
+/*        */
+/*        for (int k = i + 2 , n = strlen(s); k <= n-2; i++)*/
+/*        {*/
+/*            int */
+/*        }*/
+/*        */
+/*        for (int l = i + 3 , n = strlen(s); l <= n-3; i++)*/
+/*        {*/
+/*        */
+/*        }*/
+/*        */
+/*        for (int m = i + 4 , n = strlen(s); m <= n-4; i++)*/
+/*        {*/
